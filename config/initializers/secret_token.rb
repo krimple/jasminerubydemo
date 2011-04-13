92ce7bfefba04fd9d106b64e4b3765694991e13e
Ken Rimple <krimple@chariotsolutions.com>
2011-04-13 15:24:25 -0400

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jasminedemo::Application.config.secret_token = 'c4b465a45878fe341017877d7fa67a8416185ec1f5ba6c3b78e370ddacec8dba3c7a6e22355334c4647dac15e51c9b65580ba2339856f70d709fe0d44946c562'
